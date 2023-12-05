f88b5c733e4538a144c6166f259df64cd4eee8dd
Sevimonline <sevimonline@hotmail.com>
2023-12-05 11:43:19 +0300

    commit7

--- test_main.py
+++ test_main.py
@@ -1,27 +1,16 @@
 from fastapi.testclient import TestClient
 from main import app
 
+
+
 client = TestClient(app)
 
-def test_home():
+def test_root():
     response = client.get("/")
     assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
 
-def test_logreg_predict():
-    data = {
-        "Age": 50,
-        "Sex": 0,
-        "ChestPainType": 2,
-        "RestingBP": 135,
-        "Cholesterol": 250,
-        "FastingBS": 0,
-        "RestingECG": 2,
-        "MaxHR": 140,
-        "ExerciseAngina": 1,
-        "Oldpeak": 1.00,
-        "ST_Slope": 1
-    }
-
-    response = client.post("/pridict/logreg_model/", json=data)
+def test_read_item():
+    response = client.get("/items")
     assert response.status_code == 200
-    
+    assert response.json() == [{"name": "Foo"}, {"name": "Bar"}]
