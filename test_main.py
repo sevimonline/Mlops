970a204772b52dd679082ce3eead35eae2bdaae4
Sevimonline <sevimonline@hotmail.com>
2023-12-05 11:50:42 +0300

    commit7

--- test_main.py
+++ test_main.py
@@ -3,7 +3,26 @@ from main import app
 
 client = TestClient(app)
 
-def test_read_main():
+def test_home():
     response = client.get("/")
     assert response.status_code == 200
-    assert "Welcome" in response.text
+    assert "Heart Disease Predictor" in response.text
+
+def test_logreg_predict():
+    payload = {
+        "Age": 35,
+        "Sex": 1,
+        "ChestPainType": 2,
+        "RestingBP": 120,
+        "Cholesterol": 200,
+        "FastingBS": 0,
+        "RestingECG": 1,
+        "MaxHR": 150,
+        "ExerciseAngina": 0,
+        "Oldpeak": 1.00,
+        "ST_Slope": 1,
+    }
+
+    response = client.post("/pridict/logreg_model/", data=payload)
+    assert response.status_code == 200
+    assert "Prediction" in response.text
