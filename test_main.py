f367da82659caa0c08cf58791599b709d4b65a59
Sevimonline <sevimonline@hotmail.com>
2023-12-05 11:48:51 +0300

    commit7

--- test_main.py
+++ test_main.py
@@ -1,16 +1,9 @@
 from fastapi.testclient import TestClient
 from main import app
 
-
-
 client = TestClient(app)
 
-def test_root():
+def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
-
-def test_read_item():
-    response = client.get("/items")
-    assert response.status_code == 200
-    assert response.json() == [{"name": "Foo"}, {"name": "Bar"}]
+    assert "Welcome" in response.text
