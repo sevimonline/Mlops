cee4267b0b9826d2e31ae2cab298dd2a096dc255
Sevimonline <sevimonline@hotmail.com>
2023-12-10 01:10:01 +0300

    Final Project

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def home():
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Logistic Regression Predictor</title>
+        <link rel="stylesheet" href="/static/style.css">
         <style>
             body {
                 font-family: Arial, sans-serif;
@@ -25,7 +26,7 @@ def home():
                 align-items: center;
                 justify-content: center;
                 height: 100vh;
-                background-image: url('13up-healthlife-superJumbo-v2.gif'); /* Resmin yolunu düzeltin */
+                background-image: url('13up-healthlife-superJumbo-v2.gif'); /* Değiştirmeniz gereken yer */
                 background-size: cover;
                 background-position: center;
                 background-repeat: no-repeat;
